53c53eb1bb2a6241761127fff568e43c05b30930
P4FX LIVE CHART TRADING <p4fxmedia@gmail.com>
2026-02-27 19:54:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7678355018-7c78a",
+  "appId": "1:38175501493:web:e9a28e2aeb2d5a23e762fc",
+  "apiKey": "AIzaSyB8zDn7id3EfyBsm-VxRFPx1UaIuYFXvgw",
+  "authDomain": "studio-7678355018-7c78a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "38175501493"
+};
