d7cd82105d6f62c98164c728df4b6b77a212a705
P4FX LIVE CHART TRADING <p4fxmedia@gmail.com>
2026-03-03 00:07:44 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -33,16 +34,16 @@ const ScrollBar = React.forwardRef<
     className={cn(
       "flex touch-none select-none transition-colors duration-200",
       orientation === "vertical" &&
-        "h-full w-[14px] border-l-2 border-r-2 border-[#6366f1] p-[2px] bg-gradient-to-b from-[#ff003c] to-[#ff8a00]",
+        "h-full w-[16px] border-l-2 border-r-2 border-[#6366f1] p-[2px] bg-gradient-to-b from-[#ff003c] to-[#ff8a00]",
       orientation === "horizontal" &&
-        "h-[14px] flex-col border-t-2 border-b-2 border-[#6366f1] p-[2px] bg-gradient-to-r from-[#ff003c] to-[#ff8a00]",
+        "h-[16px] flex-col border-t-2 border-b-2 border-[#6366f1] p-[2px] bg-gradient-to-r from-[#ff003c] to-[#ff8a00]",
       "data-[state=visible]:opacity-100 data-[state=hidden]:opacity-100", 
       className
     )}
     {...props}
   >
     <ScrollAreaPrimitive.ScrollAreaThumb 
-      className="relative flex-1 rounded-full bg-[#6366f1] shadow-[0_0_20px_rgba(99,102,241,1),0_0_10px_rgba(99,102,241,0.8)]" 
+      className="relative flex-1 rounded-full bg-[#6366f1] shadow-[0_0_25px_rgba(99,102,241,1),0_0_12px_rgba(99,102,241,0.8)]" 
     />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
